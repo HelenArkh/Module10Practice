c2e29820e52a8b35b66ee2fa77f7b1de86366fb3
HelenArkh <alena.arkh@gmail.com>
2021-02-10 14:56:43 +0300

    Выполнила задание

---
+++ Module10Practice/Module10Practice/Calculate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module10Practice
+{
+    class Calculate : ICalculate
+    {
+        ILogger Logger { get; }
+        public Calculate(ILogger logger)
+        {
+            Logger = logger;
+        }
+        public int Sum(int a, int b)
+        {
+            Logger.Event("Метод Sum начал свою работу");           
+            int c = a + b;
+            Logger.Event("Метод Sum завершил свою работу");
+            return c;
+        }
+    }
+
+    public interface ICalculate
+    {
+        public int Sum(int a, int b);
+    }
+}
---
+++ Module10Practice/Module10Practice/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Module10Practice
+{
+    class Program
+    {
+        static ILogger logger { get; set; }
+        static void Main(string[] args)
+        {
+            //реалезуем интерфейс ILogger при помощи класса Logger
+            logger = new Logger();
+
+            var sum = new Calculate(logger);
+
+            try
+            {
+                Console.WriteLine("Введите первое число:");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Введите второе число:");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                int c = sum.Sum(a, b);
+                Console.WriteLine(c);
+
+            }
+            catch (Exception ex)
+            {
+                if (ex is FormatException) Console.WriteLine("Введенное значение не является целым числом!");
+                else Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Конец работы программы");
+            }            
+        }
+    }
+    public interface ILogger
+    {
+
+        void Event(string message);
+        void Error(string message);
+
+        void Write() { Console.WriteLine("Hello!"); }
+    }
+
+    //класс Logger, который будет реализовывать интерфейс ILogger
+    public class Logger : ILogger
+    {
+        void ILogger.Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Ошибка: {message}");
+        }
+
+        void ILogger.Event(string message)
+        {
+            ///добавим изменение цвета консоли
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"Событие: {message}");
+        }
+    }
+}
